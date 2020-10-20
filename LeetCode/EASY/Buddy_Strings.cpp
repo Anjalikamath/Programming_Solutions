be78d700a188ba4673c0fbafb69e1828d871e3d7
Dabb <kamathanjali1999@gmail.com>
2020-10-20 14:01:39 +0530

    Day 16

--- LeetCode/EASY/Buddy_Strings.cpp
+++ LeetCode/EASY/Buddy_Strings.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     bool buddyStrings(string A, string B) {
-        if ( A.size() != B.size() ) {
-            return false;
-        }
+        
+        if(A.size() != B.size())
+            return 0;
+        if(A.size() == 0 || B.size() == 0)
+            return 0;
         
         int freq[26] = {0};
-        int first = -1;
-        int second = -1;
-        bool noop_swap = false;
-        for (size_t i = 0; i < A.size(); ++i) {
-            if ( A[i] != B[i] ) {
-                if ( first == -1 ) {
-                    first = i;
-                } else if ( second == -1 ) {
-                    second = i;
-                } else {
-                    return false;
-                }
-            }
+        bool no_swap = false;
+        int first = -1, second = -1;
+        
+        for(int i = 0;i < A.size(); i++){
+            
+            if(A[i] != B[i]){
                 
-            ++freq[A[i] - 'a'];
-            if ( freq[A[i] - 'a'] > 1 ) {
-                    noop_swap = true;
+                if(first == -1){
+                    first = i;
+                }
+                else if( second == -1){
+                    second = i;
+                }
+                else return 0;
+                
             }
+            
+            freq[A[i]-'a']++;
+            if(freq[A[i]-'a'] > 1){
+                no_swap = true;
+            }
+             
         }
-        return (second != -1 && (A[first] == B[second] && A[second] == B[first]))
-                  || (first == -1 && noop_swap);
+        
+        return ( ((first != -1) && (second != -1) && (A[first]==B[second]) &&(A[second] == B[first])) ||
+               (first == -1 && no_swap == true) );
+        
     }
 };
